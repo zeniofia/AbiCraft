496fd63d0881ab9fabd2e95358ff59e696daa8fb
Zeni <122245538+zeniofia@users.noreply.github.com>
2026-02-02 15:36:56 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/abicraft/main.go
@@ -0,0 +1,23 @@
+// cmd/abicraft/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "abicraft/internal/abicraft"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := abicraft.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
